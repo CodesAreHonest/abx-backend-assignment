cfd3c8ac7f35fd954b0ca4a76c33d82eb532d515
Yinghua Chai <yinghuachai0@gmail.com>
2020-09-29 23:10:50 +0800

    chore: initial commit

---
+++ sql/remove_unused_table.sql
@@ -0,0 +1,7 @@
+drop table if exists Artist;
+drop table if exists Album;
+drop table if exists Customer;
+drop table if exists Employee;
+drop table if exists Invoice;
+drop table if exists InvoiceLine;
+drop table if exists Track;
