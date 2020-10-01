b317ebb3859ec9857387783822f21145e5d718a5
Yinghua Chai <yinghuachai0@gmail.com>
2020-10-02 00:12:02 +0800

    feat: entities

--- entities/Genre.ts
+++ entities/Genre.ts
@@ -1,15 +1,15 @@
 import { Column, Entity, Index, OneToMany } from "typeorm";
 import { Track } from "./Track";
 
+@Index("IPK_Genre", ["genreId"], { unique: true })
 @Entity("Genre")
 export class Genre {
-    @Index("IPK_Genre", ["genreId"], { unique: true })
-    @Column("integer", { primary: true, name: "GenreId", unique: true })
-    genreId: number;
+  @Column("integer", { primary: true, name: "GenreId", unique: true })
+  genreId: number;
 
-    @Column("nvarchar", { name: "Name", nullable: true, length: 120 })
-    name: string | null;
+  @Column("nvarchar", { name: "Name", nullable: true, length: 120 })
+  name: string | null;
 
-    @OneToMany(() => Track, (track) => track.genre)
-    tracks: Track[];
+  @OneToMany(() => Track, (track) => track.genre)
+  tracks: Track[];
 }
---
+++ entities/Track.ts
@@ -0,0 +1,60 @@
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+} from "typeorm";
+import { Playlist } from "./Playlist";
+import { Genre } from "./Genre";
+import { MediaType } from "./MediaType";
+
+@Index("IPK_Track", ["trackId"], { unique: true })
+@Index("IFK_TrackMediaTypeId", ["mediaTypeId"], {})
+@Index("IFK_TrackGenreId", ["genreId"], {})
+@Entity("Track")
+export class Track {
+  @Column("integer", { primary: true, name: "TrackId", unique: true })
+  trackId: number;
+
+  @Column("nvarchar", { name: "Name", length: 200 })
+  name: string;
+
+  @Column("integer", { name: "MediaTypeId" })
+  mediaTypeId: number;
+
+  @Column("integer", { name: "GenreId" })
+  genreId: number;
+
+  @Column("nvarchar", { name: "Composer", nullable: true, length: 220 })
+  composer: string | null;
+
+  @Column("integer", { name: "Milliseconds" })
+  milliseconds: number;
+
+  @Column("integer", { name: "Bytes", nullable: true })
+  bytes: number | null;
+
+  @Column("numeric", { name: "UnitPrice", precision: 10, scale: 2 })
+  unitPrice: number;
+
+  @ManyToMany(() => Playlist, (playlist) => playlist.tracks)
+  @JoinTable({
+    name: "PlaylistTrack",
+    joinColumns: [{ name: "TrackId", referencedColumnName: "trackId" }],
+    inverseJoinColumns: [
+      { name: "PlaylistId", referencedColumnName: "playlistId" },
+    ],
+  })
+  playlists: Playlist[];
+
+  @ManyToOne(() => Genre, (genre) => genre.tracks)
+  @JoinColumn([{ name: "GenreId", referencedColumnName: "genreId" }])
+  genre: Genre;
+
+  @ManyToOne(() => MediaType, (mediaType) => mediaType.tracks)
+  @JoinColumn([{ name: "MediaTypeId", referencedColumnName: "mediaTypeId" }])
+  mediaType: MediaType;
+}
