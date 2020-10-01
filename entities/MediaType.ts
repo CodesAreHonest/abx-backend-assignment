cf333b9750b97293d8ef830601b65a88f57daf24
Yinghua Chai <yinghuachai0@gmail.com>
2020-10-02 00:12:02 +0800

    feat: entities

--- entities/MediaType.ts
+++ entities/MediaType.ts
@@ -1,15 +1,15 @@
 import { Column, Entity, Index, OneToMany } from "typeorm";
 import { Track } from "./Track";
 
+@Index("IPK_MediaType", ["mediaTypeId"], { unique: true })
 @Entity("MediaType")
 export class MediaType {
-    @Index("IPK_MediaType", ["mediaTypeId"], { unique: true })
-    @Column("integer", { primary: true, name: "MediaTypeId", unique: true })
-    mediaTypeId: number;
+  @Column("integer", { primary: true, name: "MediaTypeId", unique: true })
+  mediaTypeId: number;
 
-    @Column("nvarchar", { name: "Name", nullable: true, length: 120 })
-    name: string | null;
+  @Column("nvarchar", { name: "Name", nullable: true, length: 120 })
+  name: string | null;
 
-    @OneToMany(() => Track, (track) => track.mediaType)
-    tracks: Track[];
+  @OneToMany(() => Track, (track) => track.mediaType)
+  tracks: Track[];
 }
